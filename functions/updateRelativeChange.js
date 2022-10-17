35af9f20abc925d04ded4255a951756e5669dc5d
MartinKratochvilProgramy <martvil96@gmail.com>
2022-10-17 17:42:25 +0200

    relative change function

--- functions/updateRelativeChange.js
+++ functions/updateRelativeChange.js
@@ -14,7 +14,7 @@ const updateRelativeChange = async (username) => {
     const totalNetWorth = stocks.netWorthHistory[stocks.netWorthHistory.length - 1].netWorth;
     const previousNetWorth = stocks.netWorthHistory[stocks.netWorthHistory.length - 2].netWorth;
     // relative change in % compared to previous net worth
-    const relativeChange = stocks.relativeChangeHistory[stocks.relativeChangeHistory - 1].relativeChange * (totalNetWorth / previousNetWorth);
+    const relativeChange = stocks.relativeChangeHistory[stocks.relativeChangeHistory.length - 1].relativeChange * (totalNetWorth / previousNetWorth);
     stocks.relativeChangeHistory.push({
         date: today,
         relativeChange: relativeChange
