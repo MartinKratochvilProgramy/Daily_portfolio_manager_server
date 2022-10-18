9b6950abdbbb78f9cebbd843c0721dc0c04f7075
martin.kratochvil <martvil96@gmail.com>
2022-10-18 12:13:28 +0200

    working on update

--- routes/update.js
+++ routes/update.js
@@ -19,22 +19,20 @@ const update = app.post("/update", async (req, res) => {
 
     let response = [];
 
-    // const passwordIsValid = await bcrypt.compare(password, user.password);
-
-    // const today = new Date();
-    // if(today.getDay() !== 6 && today.getDay() !== 0) { 
-    //   // only run on weekdays
-    //   const allStocks = await Stocks.find();
-    //   for (let i = 0; i < allStocks.length; i++) {
-    //     // await updateStocks(allStocks[i].username);
-    //     // await updateRelativeChange(allStocks[i].username);
-    //     const stocksResponse = await updateStocks(allStocks[i].username);
-    //     response.push(stocksResponse);
-    //     const relativeChangeResponse = await updateRelativeChange(allStocks[i].username);
-    //     response.push(relativeChangeResponse);
-    //   }
-    //   response.push("-------------------");
-    // }
+    const today = new Date();
+    if(today.getDay() !== 6 && today.getDay() !== 0) { 
+      // only run on weekdays
+      const allStocks = await Stocks.find();
+      for (let i = 0; i < allStocks.length; i++) {
+        // await updateStocks(allStocks[i].username);
+        // await updateRelativeChange(allStocks[i].username);
+        const stocksResponse = await updateStocks(allStocks[i].username);
+        response.push(stocksResponse);
+        const relativeChangeResponse = await updateRelativeChange(allStocks[i].username);
+        response.push(relativeChangeResponse);
+      }
+      response.push("-------------------");
+    }
 
     res.json({
       message: "Success",
