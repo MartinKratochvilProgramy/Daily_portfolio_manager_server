625ef5bb8f06a9a4f298d1a51cf491a5f624c8aa
MartinKratochvilProgramy <martvil96@gmail.com>
2022-11-24 17:14:41 +0100

    /stocks outputs formatted array for sorting

--- routes/stocks.js
+++ routes/stocks.js
@@ -22,7 +22,27 @@ const stocks = app.get("/stocks", async (req, res) => {
     const foundStocks = await Stocks.findOne({ username: username }).exec();
     if (foundStocks) {
       const stocks = foundStocks.stocks;
-      res.json(stocks);
+      const purchaseHistory = foundStocks.purchaseHistory;
+
+      const response = [];
+
+      for (let i = 0; i < stocks.length; i++) {
+        // construct array of stock objects {ticker, amount, prevClose, _id, firstPurchase, lastPurchase}
+        const stockObject = {};
+        stockObject.ticker = stocks[i].ticker;
+        stockObject.amount = stocks[i].amount;
+        stockObject.prevClose = stocks[i].prevClose;
+        stockObject._id = stocks[i]._id;
+
+        const ticker = stockObject.ticker;
+        const index = purchaseHistory.findIndex(stock => stock.ticker === ticker);
+        
+        stockObject.firstPurchase = purchaseHistory[index].purchases[0].date;                                          // first known purchase history
+        stockObject.lastPurchase = purchaseHistory[index].purchases[purchaseHistory[index].purchases.length - 1].date; // last known purchase history
+
+        response.push(stockObject)
+      }
+      res.json(response);
     } else {
       res.status(404);
       res.json({
