89fd0150033a8bd74059b3caed5d0df23654f944
martin.kratochvil <martvil96@gmail.com>
2023-01-11 10:15:45 +0100

    display data

--- middleware/ticker_chart.ts
+++ middleware/ticker_chart.ts
@@ -34,6 +34,8 @@ export default async function ticker_chart(req: Request, res: Response) {
         }
         
         const data = await getTickerChartData(ticker, period);
+
+        console.log(data);
         
         res.json(data);
 
